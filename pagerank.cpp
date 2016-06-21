eeafe32c9def012ec6934e34b9e6839bc059d728
野上和加奈 <wakana.tn16@gmail.com>
2016-06-21 17:30:23 +0900

    use range based for

--- pagerank.cpp
+++ pagerank.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <unordered_map>
+#include <map>
 #include <vector>
 
 struct Node {
@@ -12,9 +12,9 @@ struct Node {
   std::vector<std::string> m_link_to;
 };
 
-std::unordered_map<std::string, Node*> set_data(std::ifstream &ifs) {
+std::map<std::string, Node*> set_data(std::ifstream &ifs) {
   std::string line, key, link;
-  std::unordered_map<std::string, Node*> node_map;
+  std::map<std::string, Node*> node_map;
   getline(ifs, line);
   int node_num = std::stod(line, nullptr);
   for (int i = 0; i < node_num; i++) {
@@ -31,11 +31,10 @@ std::unordered_map<std::string, Node*> set_data(std::ifstream &ifs) {
   return node_map;
 }
 
-std::unordered_map<std::string, Node*> calculate_value
-(std::unordered_map<std::string, Node*> node_map) {
+std::map<std::string, Node*> calculate_value(std::map
+                                             <std::string, Node*> node_map) {
   for (auto map : node_map) {
-    double give_value =
-      map.second->m_value / map.second->m_link_to.size();
+    double give_value = map.second->m_value / map.second->m_link_to.size();
     for (auto& ref : map.second->m_link_to) {
       node_map[ref]->m_new_value += give_value;
     }
@@ -56,12 +55,12 @@ int main(int argc, char *argv[]) {
     std::cerr << "error" << std::endl;
     return -1;
   }
-  std::unordered_map<std::string, Node*> node_map = set_data(ifs);
+  std::map<std::string, Node*> node_map = set_data(ifs);
   for (int i = 0; i < 30; i++) {
     node_map = calculate_value(node_map);  
   }
   for (auto map : node_map) {
-    std::cout << map.first << map.second->m_value << std::endl;
+    std::cout << map.first + " " << map.second->m_value << std::endl;
   }
   return 0;
 }
